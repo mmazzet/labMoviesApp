6e0bed27471e2c547d887f2e79a218303ed7109f
Marianna Mazzette <20104120@mail.wit.ie>
2024-07-26 22:59:09 +0100

    fixed tvshowdetailspage

--- src/pages/tvShowDetailsPage.tsx
+++ src/pages/tvShowDetailsPage.tsx
@@ -1,61 +1,41 @@
 import React from "react";
-import TvShowHeader from "../components/headerTvShow";
+import { useParams } from "react-router-dom";
 import TvShowDetails from "../components/tvShowDetails";
-import Grid from "@mui/material/Grid";
-import ImageList from "@mui/material/ImageList";
-import ImageListItem from "@mui/material/ImageListItem";
-import { getTvShow, getTvShowImages } from "../api/tmdb-api";
-import { TvShowPageProps} from "../types/interfaces";
+import PageTemplate from "../components/templateTvShowPage";
+import { getTvShow } from "../api/tmdb-api";
+import { useQuery } from "react-query";
+import Spinner from '../components/spinner';
+import { TvShowDetailsProps } from "../types/interfaces";
 
-const styles = {
-  imageListRoot: {
-    display: "flex",
-    flexWrap: "wrap",
-    justifyContent: "space-around",
-  },
-  gridListTile: { 
-    width: "100%",
-    height: "auto",
-  },
+const TvShowDetailsPage: React.FC= () => {
+  const { id } = useParams();
+  const { data: tvShow, error, isLoading, isError } = useQuery<TvShowDetailsProps, Error>(
+    ["tvShow", id],
+    ()=> getTvShow(id||"")
+  );
 
-};
+  if (isLoading) {
+    return <Spinner />;
+  }
 
-const TvShowPage: React.FC<TvShowPageProps> = ({tvShow, images}) => {
+  if (isError) {
+    return <h1>{(error as Error).message}</h1>;
+  }
 
   return (
     <>
       {tvShow ? (
         <>
-          <TvShowHeader {...tvShow} />
-          <Grid container spacing={5} style={{ padding: "15px" }}>
-            <Grid item xs={3}>
-              <div >
-                <ImageList sx={styles.imageListRoot} cols={1}>
-                  {images.map((image) => (
-                    <ImageListItem
-                      key={image.file_path}
-                      sx={styles.gridListTile}
-                      cols={1}
-                    >
-                     <img
-                        src={`https://image.tmdb.org/t/p/w500/${image.file_path}`}
-                        alt={'Image alternative'}
-                      />                    
-                    </ImageListItem>
-                  ))}
-                </ImageList>
-              </div>
-            </Grid>
-            <Grid item xs={9}>
-              <TvShowDetails {...tvShow} />
-            </Grid>
-          </Grid>
-        </>
-      ) : (
-        <h2>Waiting for API data</h2>
-      )}
+        <PageTemplate tvShow={tvShow}> 
+          <TvShowDetails {...tvShow} />
+        </PageTemplate>
+      </>
+    ) : (
+      <p>Waiting for tvShow details</p>
+    )}
     </>
   );
 };
 
-export default TvShowPage;
+export default TvShowDetailsPage;
+
